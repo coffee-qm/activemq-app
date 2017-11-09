d9ac13308d99ef163915222edc136db36766a5e6
coffee-qm <coffee.qm@gmail.com>
2017-11-09 23:59:36 +0800

    ActiveMq App
    ActiveMq App

--- activemq-common/src/test/java/com/coffee/activemq/common/utils/EncryptUtilTest.java
+++ activemq-common/src/test/java/com/coffee/activemq/common/utils/EncryptUtilTest.java
@@ -6,13 +6,19 @@ import org.junit.Test;
  * @author QM
  */
 public class EncryptUtilTest {
+	private static String KEY = "111111";
+
 	@Test
 	public void encode() {
-		EncryptUtil.getInstance().encodeDes("", "111");
+		final String decode = "admin";
+		final String encode = EncryptUtil.getInstance().encodeDes(decode, KEY);
+		System.out.println("encode:" + encode);
 	}
 
 	@Test
 	public void decode() {
-		EncryptUtil.getInstance().decodeDes("", "111");
+		final String encode = "D820BC4E6689C4E5";
+		final String decode = EncryptUtil.getInstance().decodeDes(encode, KEY);
+		System.out.println("decode:" + decode);
 	}
 }
